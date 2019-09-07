ca06f4fed08dfd9f83e9021b4603deac37169246
Søren Sjørup <soren.sjorup@deondigital.com>
2019-09-07 10:08:47 +0200

    indent

--- cloray.clj
+++ cloray.clj
@@ -1,7 +1,7 @@
 (defn v
-    ([x] {:x x})
-    ([x y] {:x x :y y})
-    ([x y z] {:x x :y y :z z}))
+  ([x] {:x x})
+  ([x y] {:x x :y y})
+  ([x y z] {:x x :y y :z z}))
 (defn map-kv [m f] (reduce-kv #(assoc %1 %2 (f %3)) {} m))
 (defn scale [scalar vec] (map-kv vec #(* scalar %)))
 (defn vadd [a b] (merge-with + a b))
@@ -10,6 +10,6 @@
 (defn vlength [v] (Math/sqrt (vdot v v)))
 (defn norm [v] (scale (/ 1 (vlength v)) v))
 (defn dist [line point]
-    (let [ptoa (vsub (:pos line) point)
-          n (norm (:vec line))]
-        (vlength (vsub ptoa (scale (vdot ptoa n) n)))))
+  (let [ptoa (vsub (:pos line) point)
+        n (norm (:vec line))]
+    (vlength (vsub ptoa (scale (vdot ptoa n) n)))))
