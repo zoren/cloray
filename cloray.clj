71d3d6e2bedeaf42c59c0b7992678e902816b0b5
Søren Sjørup <soren.sjorup@deondigital.com>
2019-09-14 18:53:14 +0200

    make a proper render function and avoid some redundant calculations

--- cloray.clj
+++ cloray.clj
@@ -43,7 +43,7 @@
      :color (rgb 0 0 255)}
     })
 
-(def scene
+(def scene-three-balls
   {:spheres spheres
    :camera
    {:pos (v -1 -1 10)
@@ -54,39 +54,39 @@
    })
 
 (def clamp #(max 0 (min 255 %)))
-(defn calc-pixel [x y width height]
-  (let [camera (:camera scene)
-        view-plane-point
-        (vadd (:pos camera) (scale (/ x width) (:xvec camera)) (scale (/ y height) (:yvec camera)))
-        ray {:pos view-plane-point :vec (:dir camera)}
-        sphere-dists (map (fn [sphere] [(line-sphere-intersections-scale ray sphere) sphere]) spheres)
-        ;; TODO we should remove intersections that are behind the camera here
-        sorted-sphere-dists (sort-by first (filter first sphere-dists))
-        closest-sphere-pair (first (filter first sorted-sphere-dists))
-        ]
-    (if closest-sphere-pair
-      (let
-          [closest-sphere (second closest-sphere-pair)
-           intersection (walk-line (first closest-sphere-pair) ray)
-           normal (norm (vsub intersection (:center closest-sphere)))
-           dot (vdot (norm (vsub intersection (:light scene))) normal)
-           g (fn [color-component] (clamp (* dot color-component)))
-           ]
-        (mapv g (:color closest-sphere))
-        )
-      )
-    ))
+(defn render [scene]
+  (fn [{:keys [width height set-pixel]}]
+    (dotimes [y height]
+      (let [camera (:camera scene)
+            yvec-pos (vadd (:pos camera) (scale (/ y height) (:yvec camera)))]
+        (dotimes [x width]
+          (let [view-plane-point (vadd yvec-pos (scale (/ x width) (:xvec camera)))
+                ray {:pos view-plane-point :vec (:dir camera)}
+                sphere-dists (map (fn [sphere] [(line-sphere-intersections-scale ray sphere) sphere]) (:spheres scene))
+                ;; TODO we should remove intersections that are behind the camera here
+                sorted-sphere-dists (sort-by first (filter first sphere-dists))
+                closest-sphere-pair (first (filter first sorted-sphere-dists))]
+            (if closest-sphere-pair
+              (let
+                  [closest-sphere (second closest-sphere-pair)
+                   intersection (walk-line (first closest-sphere-pair) ray)
+                   normal (norm (vsub intersection (:center closest-sphere)))
+                   dot (vdot (norm (vsub intersection (:light scene))) normal)
+                   g (fn [color-component] (clamp (* dot color-component)))]
+                (set-pixel x y (mapv g (:color closest-sphere)))))))))))
 
 ;; Java specific stuff starts here
 ;; https://stackoverflow.com/a/33749052
 (defn mk-buf-img [width height] (BufferedImage. width height BufferedImage/TYPE_INT_ARGB))
 (defn rgb-to-java-color [[r g b]] (Color. (int r) (int g) (int b)))
 ;; https://www.programcreek.com/java-api-examples/?class=java.awt.image.BufferedImage&method=setRGB
+(defn set-pixel-image [image x y color]
+  (.setRGB image (int x) (int y) (.getRGB (rgb-to-java-color color))))
 (defn paint-func [image f]
   (dotimes [y (. image getHeight)]
     (dotimes [x (. image getWidth)]
       (if-let [color (f x y (. image getWidth) (. image getHeight))]
-        (.setRGB image (int x) (int y) (.getRGB (rgb-to-java-color color))))) image))
+        (set-pixel-image image x y color))) image))
 (defn mk-col [image color] (paint-func image (constantly color)))
 
 (defn write-png-file [image file-name] (ImageIO/write image "png" (File. (str "./" file-name ".png"))))
@@ -101,6 +101,11 @@
         cv (proxy [java.awt.Canvas] [] (paint [g] (. g drawImage image nil nil)))]
     (.add (.getContentPane frame) cv)
     (.revalidate frame)))
+(defn render-image [image scene]
+  ((render scene) {:width (. image getWidth)
+                   :height (. image getHeight)
+                   :set-pixel
+                   (fn [x y color] (set-pixel-image image x y color))}))
 
 (comment
   (def image (mk-buf-img 100 100))
@@ -117,7 +122,8 @@
 
   (do
     (mk-col image (rgb 0 0 0))
-    (time (paint-func image calc-pixel))
+    ((render scene-three-balls) {:width 3 :height 3 :set-pixel println})
+    (time (render-image image scene-three-balls))
     (.repaint cv)
     )
   )
